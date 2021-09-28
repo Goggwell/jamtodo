83b60b017fe82705bb0655e1a4a1d62326fab869
Samuel Yusuf <zulkarnaensamuel1@gmail.com>
2021-09-28 19:55:49 +0700

    useReducer

--- packages/www/src/pages/app.js
+++ packages/www/src/pages/app.js
@@ -5,40 +5,38 @@ import { IdentityContext } from "../../identity-context";
 import Dash from "../components/dashboard";
 
 let DashLoggedOut = props => {
-    const { identity: netlifyIdentity } = useContext(IdentityContext)
+    const { user, identity: netlifyIdentity } = useContext(IdentityContext);
 
     return (
         <Container>
-            <Flex sx={{ flexDirection: "column", padding: 3}}>
-                <Heading as="h1">Start Your Day The Right Way</Heading>
-                <Button 
+            <Flex sx={{ flexDirection: "column", padding: 3 }}>
+                <Heading as="h1">Get Stuff Done</Heading>
+                <Button
                     sx={{ marginTop: 2 }}
                     onClick={() => {
-                        netlifyIdentity.open()
+                    netlifyIdentity.open();
                     }}
                 >
                     Log In
                 </Button>
             </Flex>
         </Container>
-    )
-}
+    );
+};
 
-const app = () => {
-    const { user } = useContext(IdentityContext)
+export default props => {
+    const { user } = useContext(IdentityContext);
 
-    if(!user) {
+    if (!user) {
         return (
             <Router>
-                <DashLoggedOut path="/app" />
+            <DashLoggedOut path="/app" />
             </Router>
-        )
+        );
     }
     return (
         <Router>
             <Dash path="/app" />
         </Router>
-    )
-}
-
-export default app
+    );
+};
